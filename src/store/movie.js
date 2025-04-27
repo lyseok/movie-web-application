7ab4cd6fe33c0ece371c55f85b648ada26641d2f
YUN-SEOK LEE <seok000908@gmail.com>
2025-04-28 00:11:44 +0900

    exception

--- src/store/movie.js
+++ src/store/movie.js
@@ -5,7 +5,8 @@ const store = new Store({
   page: 1,
   pageMax: 1,
   movies: [],
-  loading: false
+  loading: false,
+  message: 'Search for the movie title!'
 });
 
 export default store
@@ -15,13 +16,25 @@ export const searchMovies = async page => {
 
   if(page === 1){
     store.state.movies = [];
+    store.state.message = '';
   }
-  let res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`);
-  const { Search, totalResults } = await res.json();
-  store.state.movies = [
-    ...store.state.movies,
-    ...Search
-  ];
-  store.state.pageMax = Math.ceil(Number(totalResults) / 10);
-  store.state.loading = false;
+
+  try {
+    const res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`);
+    const { Search, totalResults, Response, Error } = await res.json();
+    if(Response === 'True') {
+      store.state.movies = [
+        ...store.state.movies,
+        ...Search
+      ];
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+    } else {
+      store.state.message = Error;
+      store.state.pageMax = 1;
+    }
+  } catch(error) {
+    console.log('searchMovies error : ', error);
+  } finally {
+    store.state.loading = false;
+  }
 }
