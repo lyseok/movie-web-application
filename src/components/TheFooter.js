c663d40049ed622099d2b0f0a8ff799835e8f9f0
YUN-SEOK LEE <seok000908@gmail.com>
2025-04-28 23:48:13 +0900

    about, NotFound

--- src/components/TheFooter.js
+++ src/components/TheFooter.js
@@ -1,4 +1,5 @@
 import { Component } from "../core/core";
+import aboutStore from '../store/about';
 
 export default class TheFooter extends Component {
   constructor(){
@@ -7,14 +8,15 @@ export default class TheFooter extends Component {
     })
   }
   render() {
+    const { github, repository } = aboutStore.state;
     this.el.innerHTML = /* html */`
       <div>
-        <a href="https://github.com/lyseok">
+        <a href='${repository}'>
           Github Repository
         </a>
       </div>
       <div>
-        <a href="https://github.com/lyseok">
+        <a href="${github}">
           ${new Date().getFullYear()}
           YUNSEOK
         </a>
---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+import { Store } from "../core/core";
+
+export default new Store({
+  photo: 'https://api.dicebear.com/9.x/adventurer-neutral/svg?seed=Leah"',
+  name: 'LeeYunSeok',
+  email: 'seok000908@gamil.com',
+  github: 'https://github.com/lyseok',
+  repository: 'https://github.com/lyseok/movie-web-application'
+})
